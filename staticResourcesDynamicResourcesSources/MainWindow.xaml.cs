97b6ebe0963465f1419f3b77149b9d0bf5f33b25
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-25 14:53:49 +0100

    static resources, dynamic resources and sources

---
+++ staticResourcesDynamicResourcesSources/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataContextAndTheINotifyPropertyChangedInterface
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void select_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Resources["editingModes"] as InkEditingModes).EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void draw_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Resources["editingModes"] as InkEditingModes).EditingMode = InkCanvasEditingMode.Ink;
+        }
+    }
+}
