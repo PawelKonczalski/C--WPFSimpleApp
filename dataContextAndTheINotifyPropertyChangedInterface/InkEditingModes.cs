1e9a234c60fd299b0356a183ca2037568ecef0a9
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-25 14:07:41 +0100

    data context and the inotify property changed interface

---
+++ dataContextAndTheINotifyPropertyChangedInterface/InkEditingModes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Controls;
+
+namespace DataContextAndTheINotifyPropertyChangedInterface
+{
+    class InkEditingModes : INotifyPropertyChanged
+    {
+        private InkCanvasEditingMode _EditingMode;
+        public InkCanvasEditingMode EditingMode 
+        {
+            get
+            {
+                return _EditingMode;
+            }
+            set
+            {
+                _EditingMode = value;
+                raiseEventThatProperityChanged("EditingMode");
+            }
+        }
+
+        public void raiseEventThatProperityChanged(string msg)
+        {
+                if(PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(msg));
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
