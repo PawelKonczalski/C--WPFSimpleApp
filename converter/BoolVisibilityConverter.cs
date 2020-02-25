d683155d7cd475f4624baa661a35d4e2597638db
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-25 20:40:13 +0100

    converter

---
+++ converter/BoolVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace converter
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    class BoolVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+
+            if (isVisible)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+
+            if (visibility == Visibility.Collapsed)
+                return false;
+            else
+                return true;
+        }
+    }
+}
