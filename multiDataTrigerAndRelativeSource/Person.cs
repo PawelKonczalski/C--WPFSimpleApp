b72703a2408e8cc6bb0a715345699f0be82b6582
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-26 21:05:30 +0100

    multi data triger and relative source

---
+++ multiDataTrigerAndRelativeSource/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace templatesControlTemplatesContentPresenterAndTemplateBinding
+{
+    class Person
+    {
+        public string Sex { get; set; }
+        public string Name { get; set; }
+
+        public Person()
+        {
+        }
+    }
+}
