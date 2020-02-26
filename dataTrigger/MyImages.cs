efc9a5a8f24521a12f5ed0854c96dcbbcf438ef3
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-26 14:53:09 +0100

    data trigger

---
+++ dataTrigger/MyImages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTriggers
+{
+    class MyImages
+    {
+        public string pathToImage { get; set; }
+        public string nameOfImage { get; set; }
+        public bool isFound { get; set; }
+
+        public MyImages()
+        {
+            this.pathToImage = "logo.png";
+            this.nameOfImage = "Logo";
+        }
+    }
+}
