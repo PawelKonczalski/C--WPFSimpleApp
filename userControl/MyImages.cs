8fb98de9b2842bd5b5ba3e99cbdb767784c26ae9
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-26 16:15:55 +0100

    usercontrol

---
+++ userControl/MyImages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTriggers
+{
+    class MyImages
+    {
+        public string pathToImage { get; set; }
+        public string nameOfImage { get; set; }
+        public bool isFound { get; set; }
+
+        public MyImages()
+        {
+            this.pathToImage = "logo-videokurspl.png";
+            this.nameOfImage = "Logo";
+        }
+        public MyImages(string pathToImage, string nameOfImage)
+        {
+            this.pathToImage = pathToImage;
+            this.nameOfImage = nameOfImage;
+
+
+            isFound = File.Exists(Path.Combine(Directory.GetCurrentDirectory(), pathToImage));
+        }
+    }
+}
