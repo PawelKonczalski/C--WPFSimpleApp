f573947bc36d34e15d0d87f38ac3f77148601a94
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-27 17:05:33 +0100

    templateSelector

---
+++ templateSelector/GenderTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace dataGrid
+{
+    class GenderTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MaleTemplate { get; set; }
+        public DataTemplate FamaleTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            PersonData person = item as PersonData;
+
+            if (person.Sex == PersonData.Gender.Male)
+                return MaleTemplate;
+
+            return FamaleTemplate;
+        }
+    }
+}
---
+++ templateSelector/PersonData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dataGrid
+{
+    class PersonData
+    {
+        public enum Gender
+        {
+            Male,
+            Famale
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool IsSubscribedToNewsleter { get; set; }
+        public Uri Website { get; set; }
+        public Gender Sex { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
