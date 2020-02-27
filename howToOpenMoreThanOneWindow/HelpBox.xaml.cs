2f5e789307299be3520b56bce600fefe4011ac4e
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-27 22:13:11 +0100

    how to open more than one window

---
+++ howToOpenMoreThanOneWindow/HelpBox.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace menusAndBuilt_inCommands
+{
+    /// <summary>
+    /// Logika interakcji dla klasy HelpBox.xaml
+    /// </summary>
+    public partial class HelpBox : Window
+    {
+        public HelpBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
