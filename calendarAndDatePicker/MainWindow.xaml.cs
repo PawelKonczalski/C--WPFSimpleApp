92f00405fd28b4813813b32e7ea5677ef3665c6b
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-28 10:40:28 +0100

    calendar and datepicker

---
+++ calendarAndDatePicker/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendarAndDatePicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Calendar calendar = sender as Calendar;
+
+            SelectedDatesCollection selectedDates = calendar.SelectedDates;
+            /*
+            foreach (var item in selectedDates)
+            {
+                MessageBox.Show(item.ToLocalTime().ToString());
+            }
+            */
+            dateCombo.ItemsSource = selectedDates;
+        }
+    }
+}
