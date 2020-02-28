6b6760a749604d8704e277daa6a608749879c824
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-28 10:17:10 +0100

    dispatchertimer

---
+++ dispatcherTimer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace dispatcherTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer dispatcher = new DispatcherTimer();
+
+            dispatcher.Interval = TimeSpan.FromSeconds(1);
+
+            dispatcher.Tick += dateTimer;
+
+            dispatcher.Start();
+
+            actualTime.Content = DateTime.Now.ToLocalTime();
+        }
+
+        private void dateTimer(object sender, EventArgs e)
+        {
+            actualTime.Content = DateTime.Now.ToLocalTime();
+        }
+    }
+}
